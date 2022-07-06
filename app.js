cc360ee1c25c83994b996b50de5a19add30ab33d
AbdiZAk <khadar1924@gmail.com>
2022-07-06 03:15:48 -0400

    Updated routes

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors')
 
 // configure app views and static files
 const app = express();
@@ -11,17 +8,17 @@ const app = express();
 // return json
 app.use(express.json())
 
-const whitelist = ['http://127.0.0.1:3000', "http://127.0.0.1:35729"]
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  optionsSuccessStatus: 200
-}
+// const whitelist = ['http://127.0.0.1:3000', "http://127.0.0.1:35729"]
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (!origin || whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error("Not allowed by CORS"))
+//     }
+//   },
+//   optionsSuccessStatus: 200
+// }
 
 // Configure app views and static files
 app.set('view engine', 'ejs');
@@ -29,7 +26,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/css', express.static(__dirname + 'public/css'))
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/dist', 'index.html'))
 });
 
@@ -37,21 +34,18 @@ app.get('/', (req, res) => {
 // const main = require('./routes/main');
 
 // API
-const weatherApi = require('./routes/v1/weather-api')
-const autoCompleteApi = require('./routes/v1/autocomplete-api')
+// const weatherApi = require('./routes/v1/weather-api')
+// const autoCompleteApi = require('./routes/v1/autocomplete-api')
 
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 // main app route
 // app.use('/', main);
 
 // api routes that use cors
-app.use(cors(corsOptions));
-app.use('/v1/weather-api', weatherApi);
-app.use('/v1/autocomplete-api', autoCompleteApi);
+// app.use(cors(corsOptions));
+// app.use('/v1/weather-api', weatherApi);
+// app.use('/v1/autocomplete-api', autoCompleteApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
