acfc20bda94f2b21426c710f1b75317a52c4ae88
AbdiZAk <khadar1924@gmail.com>
2022-07-06 03:27:07 -0400

    Updated routes

--- app.js
+++ app.js
@@ -38,18 +38,6 @@ app.use(function(req, res, next) {
   next(createError(404))
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {proError: "Page is not found on the server"};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-  next()
-});
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/dist', 'index.html'))
 });
