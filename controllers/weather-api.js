73a184bcf136c8703db71205d9685a973cce99f5
AbdiZAk <khadar1924@gmail.com>
2022-07-07 23:48:50 -0400

    Created API controllers

--- controllers/weather-api.js
+++ controllers/weather-api.js
@@ -2,6 +2,9 @@ const axios = require('axios')
 require('dotenv').config();
 
 // get weather data
+let lat = "43.0481"
+let lon = "76.1474"
+
 async function getWeatherData(lat, lon){
     let weatherApiKey = process.env.WEATHER_API_KEY;
     let apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +
@@ -10,8 +13,8 @@ async function getWeatherData(lat, lon){
 }
 
 const getWeather = async (req, res) => {
-    const lat = req.params.lat
-    const lon = req.params.lon
+    // const lat = req.params.lat
+    // const lon = req.params.lon
     const weatherData = await getWeatherData(lat, lon).catch(err => {
         return {Error: err.stack}
     }).then(data => {
--- routes/main.js
+++ routes/main.js
@@ -13,7 +13,8 @@ router.get('/', function(req, res, next) {
     res.sendFile(path.join(__dirname, '../public/dist', 'index.html'));
 });
 
-router.get('/api/weather-api/:lat&:lon', WeatherAPI.getWeather)
+// router.get('/api/weather-api/:lat&:lon', WeatherAPI.getWeather)
+router.get('/api/weather-api/', WeatherAPI.getWeather)
 
 
 module.exports = router;
