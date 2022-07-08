e8da8a4d72e384fe219a59852b1dcd3b2ecd4d48
AbdiZAk <khadar1924@gmail.com>
2022-07-08 02:10:05 -0400

    Created API controllers

--- controllers/autocomplete-api.js
+++ controllers/autocomplete-api.js
@@ -1,30 +1,27 @@
 const fetch = require('node-fetch')
-const {re} = require("@babel/core/lib/vendor/import-meta-resolve");
+const axios = require("axios")
 
 AUTOCOMPLETE_API_KEY = "a2de75c9d4e14b87a354a37215aef683"
 
 let AutocompleteApiKey = AUTOCOMPLETE_API_KEY
 
+async function getAutocompleteData(text){
+
+    let apiUrl = `https://api.geoapify.com/v1/geocode/autocomplete?text=${text}&format=json&
+    limit=5&apiKey=${AutocompleteApiKey}`;
+    return await axios.get(apiUrl)
+}
+
 // GET weather api page.
 const getAutocomplete = async (req, res) => {
     //get weather api params
     let queryData = req.query
-    let apiUrl = `https://api.geoapify.com/v1/geocode/autocomplete?text=${queryData.text}&format=json&
-    limit=5&apiKey=${AutocompleteApiKey}`;
-
-    const autocomplete = async () => {
-        try {
-            const suggestionsResponse = await fetch(apiUrl)
-            return  await suggestionsResponse.json()
-        }catch (err){
-            return { Error: err.stack}
-        }
-    }
-    autocomplete().then(responseData => {
-        res.json(responseData)
+    const suggestions = await getAutocompleteData(queryData).catch(err => {
+        return {Error: err.stack}
+    }).then(data => {
+        return data
     })
-
-
+    res.status(200).json(suggestions.data)
 };
 
 
