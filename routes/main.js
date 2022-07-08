ac0e87715c9285431e15ed9e3e7cbe08bbb219b4
AbdiZAk <khadar1924@gmail.com>
2022-07-08 00:23:48 -0400

    Created API controllers

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const path = require("path")
-const WeatherAPI = require("../controllers/weather-api")
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
